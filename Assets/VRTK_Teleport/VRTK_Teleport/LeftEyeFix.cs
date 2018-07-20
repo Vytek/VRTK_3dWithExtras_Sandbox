d0e0f2d5585461034d15c5693dd2e1e6ece749ce
Oliver Ellmers <hi@oliverellmers.com>
2018-07-20 18:25:59 +0100

    Awesome anti aliasing

---
+++ Assets/VRTK_Teleport/VRTK_Teleport/LeftEyeFix.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftEyeFix : MonoBehaviour {
+
+	Camera _centerEyeCamera;
+
+	void Awake()
+	{
+		_centerEyeCamera = this.GetComponent<Camera>();
+		_centerEyeCamera.stereoTargetEye = StereoTargetEyeMask.Left;
+	}
+
+	void fixEyeCam()
+	{
+		_centerEyeCamera = this.GetComponent<Camera>();
+		_centerEyeCamera.stereoTargetEye = StereoTargetEyeMask.Left;
+	}
+
+	void Start () {
+		_centerEyeCamera.stereoTargetEye = StereoTargetEyeMask.Left;
+		Invoke ("fixEyeCam", 1.0f);
+	}	
+
+}
